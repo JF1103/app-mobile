2927a84d1a8424a6eebe06736e242d4a14d16d00
julian fernandez <julian.gaston.fernandez@hotmail.com>
2022-10-22 15:49:01 -0300

    screen: tareas finalizadas

--- src/components/tareasFinalizadas.js
+++ src/components/tareasFinalizadas.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import {Text} from 'react-native'
+import React from 'react';
+import {Text, View, TouchableOpacity, ScrollView, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export const TareasFinalizadas = () => {
+export const TareasFinalizadas = ({ navigation }) => {
   return (
-    <Text>tareasFinalizadas</Text>
-  )
-}
+    <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            display: 'flex',
+            direction: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#fb8c00',
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 20,
+            borderColor: '#fb8c00',
+            borderWidth: 1,
+          }}>
+          <TouchableOpacity
+            style={{position: 'absolute', left: 10}}
+            onPress={() => {
+              navigation.navigate('Home');
+            }}>
+            <Icon style={styles.icon} name="arrow-back-outline" />
+          </TouchableOpacity>
 
+          <Text style={styles.text}>Tareas Finalizadas</Text>
+        </View>
+        <ScrollView>
+
+        </ScrollView>
+      </View>
+      
+  );
+};
+
+const styles = StyleSheet.create({
+  icon: {
+    fontSize: 30,
+    color: '#FAFAFA',
+  },
+  text: {
+    fontSize: 25,
+    color: '#FAFAFA',
+    marginBottom: 5,
+  },
+});
 
 
 
